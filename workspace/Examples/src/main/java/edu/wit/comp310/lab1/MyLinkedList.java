9922bd7d520a6e93b975959aabcac37884dbd101
Jatinder Singh <singhj@wit.edu>
2014-03-21 09:49:02 -0400

    WIP on Lab 1.

--- workspace/Examples/src/main/java/edu/wit/comp310/lab1/MyLinkedList.java
+++ workspace/Examples/src/main/java/edu/wit/comp310/lab1/MyLinkedList.java
@@ -1,5 +1,20 @@
 package edu.wit.comp310.lab1;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
+import edu.wit.comp310.TreeDemo.Node;
+import edu.wit.comp310.TreeDemo.Visitor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import edu.wit.comp310.TreeDemo.Node;
+import edu.wit.comp310.TreeDemo.Visitor;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -23,18 +38,6 @@ public class MyLinkedList<ValueType> implements List<ValueType> {
 		T data;
 		Node<T> next;
 	}
-	private void loopingExample() {
-		// for (initialization; continueCondition; updateRule)
-		// for (int i = 0; i < size(); i++)
-		int index = 5;
-		int i = 0;
-		Node<ValueType> current;
-		for (current = first;
-				current != null && i < index - 1;
-				current = current.next, i++);
-		
-	}
-	
 	@Override
 	public boolean add(ValueType arg0) {
 		if (this.isEmpty()) {
@@ -48,59 +51,114 @@ public class MyLinkedList<ValueType> implements List<ValueType> {
 
 	@Override
 	public void add(int index, ValueType arg1) {
+		int i=0;
 		if (index < 0 || index > size()) {
 			throw new IndexOutOfBoundsException("WTF?!?");
 		}
-		// TODO Auto-generated method stub
-//		Node<T> newNode = new Node("hello", first);
-//		first = newNode;
+		Node<ValueType> newNode = new Node<ValueType>();
+		newNode.data=arg1;
+		Node<ValueType> current=first;
+		while(i<index)
+			current=current.next;
+		newNode.next=current.next;
+		current.next=newNode;
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends ValueType> arg0) {
-		// TODO Auto-generated method stub
+		int i=0;
+		for(ValueType e: arg0){
+			if(i==0){
+				add(e);
+				i++;
+			}
+			else{
+				Node<ValueType> newNode = new Node<ValueType>();
+				Node<ValueType> current=first;
+				newNode.data = e;
+				while(current.next!=null)
+					current=current.next;
+				newNode.next=current.next;
+				current.next=newNode;
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public boolean addAll(int arg0, Collection<? extends ValueType> arg1) {
-		// TODO Auto-generated method stub
+		int i=0;
+		for(ValueType e: arg1){
+			while(i<arg0){
+				add(e);
+				i++;
+			}
+				Node<ValueType> newNode = new Node<ValueType>();
+				Node<ValueType> current=first;
+				newNode.data = e;
+				while(current.next!=null)
+					current=current.next;
+				newNode.next=current.next;
+				current.next=newNode;
+		}
 		return false;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		first=null;
 	}
 
 	@Override
 	public boolean contains(Object arg0) {
-		// TODO Auto-generated method stub
+		Node<ValueType> current;
+		for (current = first;current != null;current = current.next){
+			if(current.data==arg0)
+				return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean containsAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		Node<ValueType> current;
+		int i = 0;
+		for(Object e: arg0){
+			for (current = first;current != null;current = current.next){
+				if(e==current.data)
+					i++;
+			}
+		}
+		if(i==arg0.size())
+			return true;
+		else
+			return false;
 	}
 
 	@Override
-	public ValueType get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public ValueType get(int arg0){
+		Node<ValueType> current = first;
+		int i=0;
+		while(i!=arg0){
+			current=current.next;
+		}
+		return current.data;
 	}
 
 	@Override
 	public int indexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+		Node<ValueType> current;
+		int i=0;
+		for (current = first;current != null;current = current.next){
+			if(current.data==arg0){
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
 		return size == 0;
 	}
 
@@ -111,13 +169,22 @@ public class MyLinkedList<ValueType> implements List<ValueType> {
 			@Override
 			public boolean hasNext() {
 				// is there an element after current?
-				return false;
+				if(current.next != null)
+					return true;
+				else
+					return false;
 			}
 			@Override
 			public ValueType next() {
 				// Advance current to next
 				// return current's data
-				return null;
+				if(hasNext()){
+					current=current.next;
+					return current.data;
+				}
+				else{
+					throw new IndexOutOfBoundsException("There's nothing left!!");
+				}
 			}
 			@Override
 			public void remove() {
@@ -128,8 +195,15 @@ public class MyLinkedList<ValueType> implements List<ValueType> {
 
 	@Override
 	public int lastIndexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+		Node<ValueType> current;
+		int i=0;
+		int index = -1;
+		for (current = first;current != null;current = current.next){
+			if(current.data==arg0)
+				index=i;
+			i++;
+		}
+		return index;
 	}
 
 	@Override
@@ -146,7 +220,13 @@ public class MyLinkedList<ValueType> implements List<ValueType> {
 
 	@Override
 	public boolean remove(Object arg0) {
-		// TODO Auto-generated method stub
+		Node<ValueType> current;
+		for (current = first;current != null;current = current.next){
+			if(current.next==arg0){
+				current.next=current.next.next;
+				return true;
+			}
+		}
 		return false;
 	}
 
